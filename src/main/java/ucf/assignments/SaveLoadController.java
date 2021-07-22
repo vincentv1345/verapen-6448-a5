f968f5c404fb21b4f63440eeae63bd32f78db4ca
Vincent <vincentverapen2012@gmail.com>
2021-07-22 01:36:25 -0400

    Added Save Controller

--- src/main/java/ucf/assignments/SaveLoadController.java
+++ src/main/java/ucf/assignments/SaveLoadController.java
@@ -1,4 +1,38 @@
 package ucf.assignments;
 
-public class SaveLoadController {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import javafx.stage.Window;
+
+import java.io.File;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+
+public class SaveLoadController implements Initializable {
+    List<String> fileNames;
+    @FXML
+    void saveFile(ActionEvent actionEvent){
+        FileChooser fileChooser = new FileChooser();
+        Stage saveStage = new Stage();
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Files", fileNames));
+        try{
+            File file = fileChooser.showSaveDialog(saveStage);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        fileNames = new ArrayList<>();
+        fileNames.add(" *.txt");
+        fileNames.add("*.json");
+        fileNames.add("*.html");
+    }
 }
